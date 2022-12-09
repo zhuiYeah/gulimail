78fba1c7534eed09e2a3a842dbedc1fd20c0cbc8
zhuiYeah <850013957@qq.com>
2022-12-09 15:49:46 +0800

    网关统一配置跨域

---
+++ gulimall-gateway/src/main/java/com/example/gateway/config/GulimallCorsConfiguration.java
@@ -0,0 +1,29 @@
+package com.example.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+//网关允许任何跨域的请求
+@Configuration
+public class GulimallCorsConfiguration {
+
+    @Bean //加入容器中
+    public CorsWebFilter corsWebFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+        //1.配置跨域
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+
+
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return new CorsWebFilter(source);
+    }
+}
